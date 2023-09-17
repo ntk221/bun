72c6d4da965aec959677de6078b6fcf87bbdd43b
kazuki nitta <92190938+ntk221@users.noreply.github.com>
2023-09-17 17:04:06 +0900

    Update main.go

--- example/migrate/migrations/main.go
+++ example/migrate/migrations/main.go
@@ -6,6 +6,9 @@ var Migrations = migrate.NewMigrations()
 
 // このディレクトリの中でマイグレーションファイルがある時は
 // その情報をMigrationsにセットする
+//
+// init関数は特別な関数で、このpackageを使用する時に実行される
+// https://go.dev/doc/effective_go#init
 func init() {
 	if err := Migrations.DiscoverCaller(); err != nil {
 		panic(err)
