06979c7378713f80106030205693754369ccb02d
kazuki nitta <92190938+ntk221@users.noreply.github.com>
2023-09-17 16:58:10 +0900

    Update main.go

--- example/migrate/migrations/main.go
+++ example/migrate/migrations/main.go
@@ -4,6 +4,8 @@ import "github.com/uptrace/bun/migrate"
 
 var Migrations = migrate.NewMigrations()
 
+// このディレクトリの中でマイグレーションファイルがある時は
+// その情報をMigrationsにセットする
 func init() {
 	if err := Migrations.DiscoverCaller(); err != nil {
 		panic(err)
