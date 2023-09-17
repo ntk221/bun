dbcb87aa7017c05218571369cc99b7c832dc1720
kazuki nitta <92190938+ntk221@users.noreply.github.com>
2023-09-17 16:55:37 +0900

    Update migrations.go

--- migrate/migrations.go
+++ migrate/migrations.go
@@ -72,48 +72,57 @@ func (m *Migrations) Add(migration Migration) {
 	m.ms = append(m.ms, migration)
 }
 
+// DiscoverCaller は現在の呼び出し元をトレースし、そのファイルが存在するディレクトリを特定します。
+// その後、そのディレクトリをルートとしてマイグレーションファイルを探索します。
+// マイグレーションの発見に成功した場合、マイグレーションリストに追加します。
 func (m *Migrations) DiscoverCaller() error {
-	dir := filepath.Dir(migrationFile())
-	return m.Discover(os.DirFS(dir))
+    // migrationFile 関数を使って現在の呼び出し元のファイルパスを取得します。
+    dir := filepath.Dir(migrationFile())
+
+    // 取得したディレクトリをルートとしてマイグレーションファイルを探索します。
+    return m.Discover(os.DirFS(dir))
 }
 
+// Discover 関数は指定されたファイルシステム `fsys` 内のマイグレーションファイルを探索し、
+// 見つかったマイグレーションに関する情報を更新します。
 func (m *Migrations) Discover(fsys fs.FS) error {
-	return fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if d.IsDir() {
-			return nil
-		}
+    return fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
+        if err != nil {
+            return err
+        }
+        if d.IsDir() {
+            return nil
+        }
 
-		if !strings.HasSuffix(path, ".up.sql") && !strings.HasSuffix(path, ".down.sql") {
-			return nil
-		}
+        if !strings.HasSuffix(path, ".up.sql") && !strings.HasSuffix(path, ".down.sql") {
+            return nil
+        }
 
-		name, comment, err := extractMigrationName(path)
-		if err != nil {
-			return err
-		}
+        name, comment, err := extractMigrationName(path)
+        if err != nil {
+            return err
+        }
 
-		migration := m.getOrCreateMigration(name)
-		if err != nil {
-			return err
-		}
+        // マイグレーションがすでに存在する場合は取得し、存在しない場合は新しく作成します。
+        migration := m.getOrCreateMigration(name)
+        if err != nil {
+            return err
+        }
 
-		migration.Comment = comment
-		migrationFunc := NewSQLMigrationFunc(fsys, path)
+        migration.Comment = comment
+        migrationFunc := NewSQLMigrationFunc(fsys, path)
 
-		if strings.HasSuffix(path, ".up.sql") {
-			migration.Up = migrationFunc
-			return nil
-		}
-		if strings.HasSuffix(path, ".down.sql") {
-			migration.Down = migrationFunc
-			return nil
-		}
+        if strings.HasSuffix(path, ".up.sql") {
+            migration.Up = migrationFunc
+            return nil
+        }
+        if strings.HasSuffix(path, ".down.sql") {
+            migration.Down = migrationFunc
+            return nil
+        }
 
-		return errors.New("migrate: not reached")
-	})
+        return errors.New("migrate: not reached")
+    })
 }
 
 func (m *Migrations) getOrCreateMigration(name string) *Migration {
@@ -136,6 +145,30 @@ func (m *Migrations) getDirectory() string {
 		return m.implicitDirectory
 	}
 	return filepath.Dir(migrationFile())
+}
+
+// migrationFile は現在の呼び出し元をトレースし、
+// 最初に "/bun/migrate." を含まない関数のファイル名を返します。
+// この関数は通常、マイグレーションファイルのパスを特定するために使用されます。
+// もし呼び出し元が "/bun/migrate." を含む関数であれば、
+// 呼び出し元のファイルパスを返します。
+func migrationFile() string {
+    const depth = 32
+    var pcs [depth]uintptr
+    n := runtime.Callers(1, pcs[:])
+    frames := runtime.CallersFrames(pcs[:n])
+
+    for {
+        f, ok := frames.Next()
+        if !ok {
+            break
+        }
+        if !strings.Contains(f.Function, "/bun/migrate.") {
+            return f.File
+        }
+    }
+
+    return ""
 }
 
 func migrationFile() string {
